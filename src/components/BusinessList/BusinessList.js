440ad243de6ed492893c0307d4cdf4e94d62bcb4
Filip <filiphcvb@gmail.com>
2022-02-08 19:26:59 +0100

    initmessage

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import { buildQueries } from '@testing-library/react';
+import React from 'react';
+import './businessList.css';
+import Business from '../Business/business';
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.businesses.map((business) => {
+                        return <Business business = {business} />
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default BusinessList;
